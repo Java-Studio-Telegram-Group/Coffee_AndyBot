12744da72f04eacd901b5465eda588b10229f212
Turi Tyrsson <asemeu7@gmail.com>
2017-07-11 22:32:41 +0200

    Arreglado algunos problemas con mavens y el mensaje de bienvenida

--- src/main/java/com/mycompany/coffeeandybot/CoffeeAndyBot.java
+++ src/main/java/com/mycompany/coffeeandybot/CoffeeAndyBot.java
@@ -20,14 +20,14 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
  * @author turi
  */
 public class CoffeeAndyBot extends TelegramLongPollingBot {
-    
+
     // Este método debe devolver el token de nuestro bot para que 
     // conecte de manera correcta con telegram y pueda manejar los mensajes
     @Override
     public String getBotToken() {
         return "TOKEN-BOT";
     }
-    
+
     // En este metodo va toda la logica del bot. Es donde nuestro bot
     // maneja los mensajes que lee y actua en funcion de lo que programemos.
     @Override
@@ -36,20 +36,22 @@ public class CoffeeAndyBot extends TelegramLongPollingBot {
             Message message = update.getMessage();
             System.out.println(message);
             List<User> newUsers = update.getMessage().getNewChatMembers();
-            String welcomeMessage = "Bienvenido al grupo Java Studio: ";
-            for (User newUser : newUsers) {
-                String user = newUser.getUserName() == null ? newUser.getFirstName()
-                        : newUser.getUserName();
-                welcomeMessage += user + " ";
-            }
+            if (newUsers != null) {
+                String welcomeMessage = "Bienvenido al grupo Java Studio: ";
 
-            SendMessage welcomeSendMessage   = new SendMessage()
-                    .setChatId(message.getChatId())
-                    .setText(welcomeMessage);
-            try {
-                sendMessage(welcomeSendMessage  );
-            } catch (TelegramApiException ex) {
-                Logger.getLogger(CoffeeAndyBot.class.getName()).log(Level.SEVERE, null, ex);
+                for (User newUser : newUsers) {
+                    String user = newUser.getUserName().equals("null") ? newUser.getFirstName()
+                            : "@" + newUser.getUserName();
+                    welcomeMessage += user + " ";
+                }
+                SendMessage welcomeSendMessage = new SendMessage()
+                        .setChatId(message.getChatId())
+                        .setText(welcomeMessage);
+                try {
+                    sendMessage(welcomeSendMessage);
+                } catch (TelegramApiException ex) {
+                    Logger.getLogger(CoffeeAndyBot.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
         }
